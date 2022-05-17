efce61edb49f5a4ea81d39850d27b4b18bd2185e
suma <21gsuma@gmail.com>
2022-05-18 03:54:06 +0530

    trying to add map function for emogies

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -6,21 +6,45 @@ const Main = () =>{
     const [data, setData] = useState(null);
     const [print, setPrint] = useState(false);
 
-    function getData(event){
-        setData(event.target.value)
-        console.log(event.target.value)
+    const emogis = [
+        {
+            id:'::happy::',n: '🙂'
+        },
+        {
+            id:'::sad::',n: '😢'
+        },
+        {
+            id:'::smiley::',n: '😀'
+        }
+    ]
 
+    function getData(event){
+             
+        setData(event.target.value)
+        // console.log(event.target.value)
+       
+    }
+    function displayOutput(data){
+        setPrint(true);
+        for(let i=0;i<data.length;i++){       
+                console.log(data);  
+                if(data[i] == ':') {
+                    
+                }     
+        }
+
+        console.log(emogis)
     }
 
     return(
         <div>
         <input type="text" onChange={getData}/>
            
-            
-            <button onClick={() => setPrint(true)}>Output</button>
+        {/* setPrint(true) */}
+            <button onClick={() => displayOutput(data)}>Output</button>
             <div>
             {
-                print?
+                print?               
                 <h1>{data}</h1>
                 :null
             }
